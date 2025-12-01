84519e32c912c599c99d943d34deb6d834497704
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2025-12-01 09:20:36 -0500

    creating the movies object

--- site.js
+++ site.js
@@ -1,45 +1,86 @@
-/* SITE.JS: THIS FILE CONTAINS THE METHODS/FUNCTIONS AND VARIABLES CONTROLLING YOUR SITE
-//
-*/
-
-/* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
-//
-//    They are in the following format:
-//    title (String): the name of the movie
-//    iscore (Number): the IMDB score
-//    rating (String): the movie's MPAA rating
-//    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
-//    country (String): the country of production
-//    posters (Array): an array of String values with the URL to movie posters (in your img/ directory)
-//    imdb (String): the URL to the corresponding IMDB website
-//    website (String): the URL to the corresponding official website
-//    likes (Number): a fictitious number of user likes
-//    dislikes (Number): a fictitious number of user dislikes
-//    posterindex (Number): a counter to use with the "posters" array to keep track of the current displayed poster index
-//
-// FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
-*/
-
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
+  async created() {
+    const imported = await (await fetch("movies.json")).json();
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    this.movies = imported.map(
+      ({
+        title,
+        iscore,
+        rating,
+        runtime,
+        released: [day, month, year],
+        country,
+        posters,
+        imdb,
+        website,
+        likes,
+        dislikes,
+        posterIndex,
+      }) => {
+        console.log({
+          title,
+          iscore,
+          rating,
+          runtime: `${Math.floor(runtime / 60)} hrs ${runtime % 60} min`,
+          released: `${months[month]} ${day}${(() => {
+            const num = day.toString().split("").at(-1);
+            if (num === "1") return "st";
+            else if (num === "2") return "nd";
+            else if (num === "3") return "rd";
+            else return "th";
+          })()}, ${year}`,
+          country,
+          posters,
+          imdb,
+          website,
+          likes,
+          dislikes,
+          posterIndex,
+        });
         return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
+          title,
+          iscore,
+          rating,
+          runtime: `${Math.floor(runtime / 60)} hrs ${runtime % 60} min`,
+          released: `${months[month]} ${day}${(() => {
+            const num = day.toString().split("").at(-1);
+            if (num === "1") return "st";
+            else if (num === "2") return "nd";
+            else if (num === "3") return "rd";
+            else return "th";
+          })()}, ${year}`,
+          country,
+          posters,
+          imdb,
+          website,
+          likes,
+          dislikes,
+          posterIndex,
+        };
       }
-    },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
-})
+    );
+  },
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  methods: {
+    /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+  },
+});
 
-vue_app.mount("#vue_app")
+vue_app.mount("#vue_app");
